deca5bdce082e9bebeacedb18f7b66a1dcec4b92
kurando <kurando.iida@gmail.com>
2025-12-24 13:43:15 +0900

    fix: remove inner centered card layout in popup

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,9 @@ import App from './App.tsx'
 import Popup from './Popup.tsx'
 
 const isPopup = window.location.hash.startsWith('#/popup')
+if (isPopup) {
+  document.documentElement.classList.add('popup')
+  document.body.classList.add('popup')
+}
 
 createRoot(document.getElementById('root')!).render(isPopup ? <Popup /> : <App />)
